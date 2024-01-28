5d3181be98bec1eb6fd23108c40d9f8d45f676eb
JieniPon <157961099+JieniPon@users.noreply.github.com>
2024-01-28 13:10:53 +0100

    Update remind_me() function.R

--- remind_me() function.R
+++ remind_me() function.R
@@ -1,4 +1,3 @@
 remind_me <- function(){
-  print("Bungee Gum is made of both bungee and gum",
-        "Also, we still didn't receive our GRP grades yet.")
+  print("Bungee Gum is made of both bungee and gum")
 }
